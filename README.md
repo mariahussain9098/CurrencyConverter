100d0c274562ecee96e2586cfd9705b0850dece1
Maria <mariahussain9098@gmail.com>
2024-02-26 11:39:39 +0500

    files added

---
+++ README.md
@@ -0,0 +1,16 @@
+## Currency Converter CLI Project
+This is a simple command-line tool for currency conversion.
+
+## Usage
+- Upon running the script, you'll be prompted to select your base currency, target currency, and enter the amount you wish to convert.
+- The tool will then calculate and display the converted amount.
+
+## Supported Currencies
+Currently, the tool supports conversion between the following currencies:
+
+- PKR (Pakistani Rupee)
+- USD (United States Dollar)
+- GBP (British Pound Sterling)
+
+## Conversion Rates
+Conversion rates are hardcoded within the script for simplicity. In a real-world scenario, you would fetch these rates from an external API.
---
+++ index.js
@@ -0,0 +1,45 @@
+import inquirer from "inquirer";
+const conversion = {
+    "PKR": {
+        "USD": 0.0036,
+        "GBP": 0.0028,
+        "PKR": 1
+    },
+    "GBP": {
+        "USD": 1.27,
+        "PKR": 354.19,
+        "GBP": 1
+    },
+    "USD": {
+        "PKR": 279.65,
+        "GBP": 0.79,
+        "USD": 1
+    }
+};
+const answer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["PKR", "USD", "GBP"],
+        message: "select your currency"
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "USD", "GBP"],
+        message: "select your conversion currency"
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "enter your conversion amount"
+    }
+]);
+const { from, to, amount } = answer;
+if (from && to && amount) {
+    const result = conversion[from][to] * amount;
+    console.log(`Your conversion from ${from} to ${to} is : ${result}`);
+}
+else {
+    console.log("Invalid inputs");
+}
